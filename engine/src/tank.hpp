3050c3a72fa5cb6a0ea5f44a4722e54f4b4b2773
gtoombs <gtoombs@lakeheadu.ca>
2009-10-13 01:24:00 +0000

    GetDistanceTo, Think, tank headings.

--- engine/src/tank.hpp
+++ engine/src/tank.hpp
@@ -13,4 +13,16 @@
 /// else's.
 class Tank : public ArenaObject
 {
+public:
+	/// \return The tank's heading, in radians counterclockwise from east.
+	double GetHeading() const { return heading; }
+
+	/// \return The heading to another object relative to this tank, in radians
+	///         counterclockwise from east.
+	double GetHeadingTo(const ArenaObject &other) const {
+		return heading - GetAngleTo(other);
+	}
+private:
+	/// The tank's heading, in radians counterclockwise from east.
+	double heading;
 };
