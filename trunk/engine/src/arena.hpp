daa1cbb0803d1e57e2359c4f207248de21493d4b
gtoombs@lakeheadu.ca <gtoombs@lakeheadu.ca@9ded1eee-b4fa-11de-9b33-83dc4fd2bb11>
2009-10-13 00:32:13 +0000

    Added arena and another arenaobject method

---
+++ trunk/engine/src/arena.hpp
@@ -0,0 +1,17 @@
+/** \file
+* \par Subversion URL:
+* \$URL$
+* \version \$Rev$
+* \date    \$Date$
+*/
+
+#pragma once
+
+#include "bullet.hpp"
+#include "tank.hpp"
+
+/// The class representing a battle arena and all of the objects that it
+/// contains.
+class Arena
+{
+};
