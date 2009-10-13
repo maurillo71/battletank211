e1ab7040142c6ea48e33b3a570d99ac0190f2ccd
gtoombs@lakeheadu.ca <gtoombs@lakeheadu.ca@9ded1eee-b4fa-11de-9b33-83dc4fd2bb11>
2009-10-13 00:14:32 +0000

    First commit. Skeletal class structure for engine, doxyfile.

---
+++ trunk/engine/src/tank.cpp
@@ -0,0 +1,8 @@
+/** \file
+* \par Subversion URL:
+* \$URL$
+* \version \$Rev$
+* \date    \$Date$
+*/
+
+#include "tank.hpp"
---
+++ trunk/engine/src/tank.hpp
@@ -0,0 +1,16 @@
+/** \file
+* \par Subversion URL:
+* \$URL$
+* \version \$Rev$
+* \date    \$Date$
+*/
+
+#pragma once
+
+#include "arenaobject.hpp"
+
+/// This class represents any tank, regardless of whether it's yours or someone
+/// else's.
+class Tank : public ArenaObject
+{
+};
